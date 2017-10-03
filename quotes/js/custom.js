f44c29d9d1b8ce886a0615a0783668da1151524f
miloshsk <miloshsk@gmail.com>
2017-10-03 22:22:23 +0300

    Update custom.js

--- quotes/js/custom.js
+++ quotes/js/custom.js
@@ -3,14 +3,20 @@ $(document).ready(function() {
 	$('.hide-menu').click(function(){
  	 	$(this).toggleClass('active-menu');
  	 	$('.overlay').toggleClass('overlay-show');
+ 	 	$('.header-top-wrapper-scroll').toggleClass('header-top-wrapper-color');
+ 	 	$('.header-logo').toggleClass('header-logo-display');
 	});
 	$('.overlay').click(function(){
  	 	$(this).toggleClass('overlay-show');
  	 	$('.hide-menu').toggleClass('active-menu');
+ 	 	$('.header-top-wrapper-scroll').toggleClass('header-top-wrapper-color');
+ 	 	$('.header-logo').toggleClass('header-logo-display');
 	});
 	$('.header-main-menu-link').click(function(){
 		$('.overlay').toggleClass('overlay-show');
 		$('.hide-menu').toggleClass('active-menu');
+		$('.header-top-wrapper-scroll').toggleClass('header-top-wrapper-color');
+		$('.header-logo').toggleClass('header-logo-display');
 	})
 	$('ul.header-main-menu a').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -31,14 +37,23 @@ $(document).ready(function() {
 	function scrollFunction() {
 	    if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
 	        document.getElementById("up-button").style.display = "block";
-	    } else {
-	        document.getElementById("up-button").style.display = "none";
+	    } else if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+	    	document.querySelector("#header-top-wrapper").classList.add('header-top-wrapper-scroll');
+	    	document.querySelector(".header-logo").classList.add('header-logo-scroll');
+	    	document.querySelector(".hide-menu").classList.add('hide-menu-scroll');
+	    } else if(document.body.scrollTop < 150 || document.documentElement.scrollTop < 150)  {
+	    	document.querySelector("#header-top-wrapper").classList.remove('header-top-wrapper-scroll');
+	    	document.querySelector(".header-logo").classList.remove('header-logo-scroll');
+	    	document.querySelector(".hide-menu").classList.remove('hide-menu-scroll');
+		}
+	   	else {
+	      	document.getElementById("up-button").style.display = "none";
 	    }
 	}
 	 $('#up-button').on('click', function () {
             $('html,body').animate({
                 scrollTop: 0
-            }, 500);
+            }, 700);
         });
 	// Slider	
 	 $('.slider').slick({
