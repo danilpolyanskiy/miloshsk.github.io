1582fcaee2d17491d70318f197d7ae6e194c6ed8
miloshsk <miloshsk@gmail.com>
2017-11-29 01:01:04 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,4 @@
 $(document).ready(function() { 
-//Mobile menu
-	$('.hide-menu').click(function() {
-		$(this).toggleClass('active-menu'),
-		$('.menubar').toggleClass('menubar-mobile')$(document).ready(function() { 
 // Animate 
     wow = new WOW( {
       mobile: false
@@ -45,49 +41,4 @@ $(document).ready(function() {
 		var targetOffset = $('section.portfolio').offset().top-105;
 		$('html,body').animate({scrollTop: targetOffset}, 1000);
 	});
-});
-,
-		$('.mainmenu').toggleClass('mainmenu-hide')
-	});
-
-	$('.mainmenu-link').click(function() {
-		$('.mainmenu').toggleClass('mainmenu-hide'),
-		$('.hide-menu').toggleClass('active-menu')
-	});
-	//Scrolling		   
-	$('ul.mainmenu a').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var $target = $(this.hash);
-			$target = $target.length && $target || $('[id=' + this.hash.slice(1) +']');
-			if ($target.length) {
-				$('ul.mainmenu li').removeClass('active');
-				$(this).parent('li').addClass('active');
-				var targetOffset = $target.offset().top-100;
-				$('html,body').animate({scrollTop: targetOffset}, 1000);
-				return false;
-			}
-		}
-	});
-	//Scroll top
-	 $('#home').click(function () {
-        $('body,html').animate({
-           scrollTop: 0
-        }, 800);
-        return false;
-       });
-	//ScrollLink
-	$('.header-button-area').click(function() {
-		var targetOffset = $('section.portfolio').offset().top-105;
-		$('html,body').animate({scrollTop: targetOffset}, 1000);
-	});
-	//Scroll
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-	    if(document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-	    	document.querySelector('.ai-l').classList.add('slideInLeft');
-	    	document.querySelector(".ai-m").classList.add('slideInUp');
-	    	document.querySelector(".ai-r").classList.add('slideInRight');
-	    }
-	}
 });
