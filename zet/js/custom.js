5be4e1676e8c46cf2cf3bd5b274f34cc482f8cfd
miloshsk <miloshsk@gmail.com>
2018-01-29 01:31:00 +0300

    Update custom.js

--- zet/js/custom.js
+++ zet/js/custom.js
@@ -9,10 +9,35 @@ $(document).ready(function(){
           slidesToScroll: 1
         }
     );
-    // Menu
+    // // Menu
     $('.header-menu-button-wrapper').click(function() {
      $('.page-nav').toggleClass('page-nav-show'),
-      $('.header-nav-list').toggleClass('active-menu')
+     $('.header-nav-list').toggleClass('active-menu'),
+     $('.header-menu-button').toggleClass('header-menu-button-active')
     });
+
+    $('.nav-list a').click(function() {
+      $('.header-menu-button').toggleClass('header-menu-button-active'),
+      $('.page-nav').toggleClass('page-nav-show')
+    });
+    //Scrolling      
+  $('.nav-list a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target || $('[id=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        $('.nav-list a').removeClass('active');
+        $(this).parent('li').addClass('active');
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, 1000);
+        return false;
+      }
+    }
+  });
+    //ScrollLink
+     $('.header-button-area').click(function() {
+    var targetOffset = $('.services-items-wrapper').offset().top-225;
+    $('html,body').animate({scrollTop: targetOffset}, 1200);
+  });
 });
 	
