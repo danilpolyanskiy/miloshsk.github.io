29cecf1068a98a296176fb7673d1d6bb4ba36e49
miloshsk <miloshsk@gmail.com>
2018-03-18 15:00:09 +0300

    update

--- mstore/main.js
+++ mstore/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-	$('slider').slick();
-});
+const menuBtn = document.querySelector('.hide-menu');
+const headerNav = document.querySelector('.header-nav');
+
+menuBtn.addEventListener('click', function() {
+	headerNav.classList.toggle('header-nav-mobile'),
+	menuBtn.classList.toggle('hide-menu-active');
+})
+
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if(scrolled >= 115){
+  	headerNav.style.top = 0 + 'px';
+  } else if (scrolled <= 115) {
+  	headerNav.style.top = 115 - scrolled + 'px';
+  }
+}
