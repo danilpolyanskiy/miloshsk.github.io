c68aab48e623d4d25136c2a9af347ce000ba1458
miloshsk <miloshsk@gmail.com>
2018-03-25 21:37:11 +0400

    upd

--- mstore/main.js
+++ mstore/main.js
@@ -5,12 +5,3 @@ menuBtn.addEventListener('click', function() {
 	headerNav.classList.toggle('header-nav-mobile'),
 	menuBtn.classList.toggle('hide-menu-active');
 })
-
-window.onscroll = function() {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if(scrolled >= 115){
-  	headerNav.style.top = 0 + 'px';
-  } else if (scrolled <= 115) {
-  	headerNav.style.top = 115 - scrolled + 'px';
-  }
-}
