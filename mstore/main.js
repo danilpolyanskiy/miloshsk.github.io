bbeff2a13be8816296d0c6a519590ff69b382c90
miloshsk <miloshsk@gmail.com>
2018-04-09 00:31:20 +0400

    upd

--- mstore/main.js
+++ mstore/main.js
@@ -5,3 +5,54 @@ menuBtn.addEventListener('click', function() {
 	headerNav.classList.toggle('header-nav-mobile'),
 	menuBtn.classList.toggle('hide-menu-active');
 })
+
+const loginBtn = document.querySelector('.profile');
+const popup = document.querySelector('.login-popup');
+const loginCloseBtn = document.querySelector('.popup-close-button');
+
+loginBtn.addEventListener('click', function() {
+	popup.classList.toggle('popup-show');
+})
+ 
+loginCloseBtn.addEventListener('click', function() {
+	if (popup.classList.contains('popup-show')) {
+		popup.classList.remove('popup-show');
+	}
+})
+let basket = document.querySelector('.header-basket-value');
+let basketValue = +basket.textContent.slice(1);
+let addToBasket = document.querySelectorAll('.basket');
+const basketShow = document.querySelector('.basket-items');
+const basketBtn = document.querySelector('.header-basket');
+
+for(let i = 0; i < addToBasket.length; i++) {
+	addToBasket[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		let newValue = Math.round(this.closest('.product-item').getAttribute('data-cost'));
+		basketValue += newValue;
+		basket.innerHTML = `$${basketValue}.00`;
+
+		let li = document.createElement('li');
+		li.innerHTML = `${this.closest('.product-item').getAttribute('data-name')} : $${newValue}.00`;
+		basketShow.appendChild(li);
+	})
+}
+
+basketBtn.addEventListener('click', function() {
+	basketShow.classList.toggle('basket-items-show');
+})
+const favoriteItems = document.querySelector('.favorite-items');
+const headerFavBtn = document.querySelector('.header-favorites');
+headerFavBtn.addEventListener('click', function() {
+	favoriteItems.classList.toggle('favorite-items-show');
+})
+
+let addToFavorite = document.querySelectorAll('.favorites');
+for(let i = 0; i < addToFavorite.length; i++) {
+	addToFavorite[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		let li = document.createElement('li');
+		li.innerHTML = `${this.closest('.product-item').getAttribute('data-name')}`;
+		favoriteItems.appendChild(li);
+	})
+}
