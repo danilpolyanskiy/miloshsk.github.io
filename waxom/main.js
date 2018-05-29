caba2529e104829b3659e3dd1837f85357e855f5
miloshsk <miloshsk@gmail.com>
2018-05-29 18:24:27 +0400

    upd mobile

--- waxom/main.js
+++ waxom/main.js
@@ -3,13 +3,42 @@ $(document).ready(function(){
   	dots: true,
   	speed: 1100,
   	fade: true,
-  	cssEase: 'ease-in-out'
+  	cssEase: 'ease-in-out',
+    responsive: [
+      {
+        breakpoint: 550,
+        settings: {
+           arrows: false
+        }
+      }
+    ]
   });
    $('.posts-items-wrapper').slick({
   	dots: false,
   	speed: 1100,
   	slidesToShow: 3,
-  	slidesToScroll: 1
+  	slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 515,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+   $('.burger').click(function() {
+      $(this).toggleClass('burger-opened');
+      $('.header-nav').toggleClass('mobile-menu');
+   })
 });
 var mixer = mixitup('.projects-container');
+if(window.innerWidth <= 400) {
+  mixer.destroy();
+}
